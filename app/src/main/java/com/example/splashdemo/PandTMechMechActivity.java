914bd714a7f030c058033d5d1f6a342f78a9a522
SajanGeorg <sajangeorge010@gmail.com>
2019-12-28 14:23:45 +0530

    Done with Mech Activity.

---
+++ app/src/main/java/com/example/splashdemo/PandTMechMechActivity.java
@@ -0,0 +1,77 @@
+package com.example.splashdemo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PandTMechMechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_t_mech);
+        ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+
+        TabLayout mTabLayout = findViewById(R.id.tabLayout);
+        ViewPager mViewPager = findViewById(R.id.viewPager);
+
+       setmViewPager(mViewPager);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+       private void setmViewPager(ViewPager viewPager){
+        viewPagerAdapter adapter=new viewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PTMechFragmentAbout() ,"About");
+        adapter.addFragment(new PTMechFragmentGalllery(),"Gallery");
+        adapter.addFragment(new PTMechFragmentMap(),"Map");
+
+        viewPager.setAdapter(adapter);
+
+
+    }
+class viewPagerAdapter extends FragmentPagerAdapter{
+    private  final List<Fragment> mFragmentList=new ArrayList<>();
+    private  final List<String> mFragmentTitleList =new ArrayList<>();
+    viewPagerAdapter(FragmentManager fn){
+        super(fn);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+    void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+
+    }
+}
+}
---
+++ app/src/main/java/com/example/splashdemo/SagarMechFragmentGalllery.java
@@ -0,0 +1,30 @@
+package com.example.splashdemo;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SagarMechFragmentGalllery extends Fragment {
+
+
+    public SagarMechFragmentGalllery() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sagar_mech_galllery, container, false);
+    }
+
+}
