4e93b322a76c8eddc389a226dbf06924bfc5a1d6
SajanGeorg <sajangeorge010@gmail.com>
2019-12-30 14:14:30 +0530

    Done with First 4 cards

--- app/src/main/java/com/example/splashdemo/Gasstation.java
+++ app/src/main/java/com/example/splashdemo/Gasstation.java
@@ -1,14 +1,65 @@
 package com.example.splashdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Gasstation extends AppCompatActivity {
+public class Gasstation extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gasstation);
+        CardView mahacard = findViewById(R.id.mahanagarpump);
+        mahacard.setOnClickListener(this);
+        CardView bharatcard = findViewById(R.id.bharartpet);
+        bharatcard.setOnClickListener(this);
+        CardView usmacard = findViewById(R.id.usmapet);
+        usmacard.setOnClickListener(this);
+        CardView Technocard = findViewById(R.id.Technopet);
+        Technocard.setOnClickListener(this);
+        CardView HPpetcard = findViewById(R.id.HPpet);
+        HPpetcard.setOnClickListener(this);
+        CardView bharatpetrcard = findViewById(R.id.Bharatpetroleum);
+        bharatpetrcard.setOnClickListener(this);
+        CardView ravipetcard = findViewById(R.id.Ravipet);
+        ravipetcard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        if (view.getId() == R.id.mahanagarpump) {
+            i = new Intent(this, MahanagarCNGActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.bharartpet) {
+            i = new Intent(this, BharatCNGActivity.class);
+            startActivity(i);
+        }
+
+        if (view.getId() == R.id.usmapet) {
+            i = new Intent(this, UsmaCNGActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.Technopet) {
+            i = new Intent(this, TechnoPetActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.HPpet) {
+            i = new Intent(this, HPPetrActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.Bharatpetroleum) {
+            i = new Intent(this, BharatPetroleumActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.Ravipet) {
+            i = new Intent(this, RaviPetroleumActivity.class);
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/splashdemo/Police.java
+++ app/src/main/java/com/example/splashdemo/Police.java
@@ -1,14 +1,65 @@
 package com.example.splashdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Police extends AppCompatActivity {
+public class Police extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_police);
+        CardView thakcard = findViewById(R.id.thakpolice);
+        thakcard.setOnClickListener(this);
+        CardView thakpolcard = findViewById(R.id.thakpoilcestation);
+        thakpolcard.setOnClickListener(this);
+        CardView manpadacard = findViewById(R.id.manpadapolice);
+        manpadacard.setOnClickListener(this);
+        CardView rtocard = findViewById(R.id.RTODomb);
+        rtocard.setOnClickListener(this);
+        CardView traffichcard = findViewById(R.id.trafficchowk);
+        traffichcard.setOnClickListener(this);
+        CardView rtokalcard = findViewById(R.id.rtokalyan);
+        rtokalcard.setOnClickListener(this);
+        CardView deptrtocard = findViewById(R.id.deputykalyan);
+        deptrtocard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        if (view.getId() == R.id.thakpolice) {
+            i = new Intent(this,ThakurliOutpoliceActivity.class);
+            startActivity(i);
+        }
+       if (view.getId() == R.id.thakpoilcestation) {
+            i = new Intent(this, ThakurliPoliceActivity.class);
+            startActivity(i);
+        }
+
+         if (view.getId() == R.id.manpadapolice) {
+            i = new Intent(this, ManpadaPoliceActivity.class);
+            startActivity(i);
+        }
+       if (view.getId() == R.id.RTODomb) {
+            i = new Intent(this, RTODombActivity.class);
+            startActivity(i);
+        }
+         if (view.getId() == R.id.trafficchowk) {
+            i = new Intent(this, TrafficchowkActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.rtokalyan) {
+            i = new Intent(this, RTOKalyanActivity.class);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.deputykalyan) {
+            i = new Intent(this, DeputyKalyanActivity.class);
+            startActivity(i);
+        }
     }
 }
