4b74cd400d8c384baf6e5f34aa255b955b336784
Amol <amolsbhalerao11@gmail.com>
2020-02-21 13:15:56 +0530

    All the hospital activities are added

---
+++ app/src/main/java/com/example/splashdemo/KamatHospitalActivity.java
@@ -0,0 +1,79 @@
+package com.example.splashdemo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KamatHospitalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kamat_hospital);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+
+        TabLayout mTabLayout = findViewById(R.id.tabLayout);
+        ViewPager mViewPager = findViewById(R.id.viewPager);
+
+        setmViewPager(mViewPager);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setmViewPager(ViewPager viewPager) {
+        viewPagerAdapter adapter = new viewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new kamat_fragment_about(), "About");
+        adapter.addFragment(new kamat_fragment_gallery(), "Gallery");
+        adapter.addFragment(new kamat_fragment_map(), "Map");
+
+        viewPager.setAdapter(adapter);
+
+
+    }
+
+    class viewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        viewPagerAdapter(FragmentManager fn) {
+            super(fn);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int i) {
+            return mFragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+
+        }
+    }
+}
